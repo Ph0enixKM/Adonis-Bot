ac7bcabb63b0b1eec81c87f35a7583fa95cea49c
Ph0enixKM <pkaras.it@gmail.com>
2023-02-17 19:57:01 +0100

    feat: word reactions

--- app.js
+++ app.js
@@ -2,33 +2,34 @@ const { Client, GatewayIntentBits, ActivityType } = require("discord.js");
 require('dotenv').config()
 
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-  ],
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ],
 });
 
 const token = process.env.TOKEN
 
 const getEmojiByName = (message, name) => {
-  const emoji = message.guild.emojis.cache.find((emoji) => name === emoji.name);
-  return emoji ? `<:${name}:${emoji.id}>` : null;
+    const emoji = message.guild.emojis.cache.find((emoji) => name === emoji.name);
+    return emoji ? `<:${name}:${emoji.id}>` : null;
 };
 
 client.on("ready", () => {
-  client.user.setPresence({
-    activities: [{ name: `meditation`, type: ActivityType.Competing }],
-  });
-  client.user.setStatus("online");
-  console.log("connected");
+    client.user.setPresence({
+        activities: [{ name: `meditation`, type: ActivityType.Competing }],
+    });
+    client.user.setStatus("online");
+    console.log("connected");
 });
 
 client.on("messageCreate", (message) => {
-  const chad = getEmojiByName(message, "chad");
-  if (message.content == "adonis") {
-    if (chad) message.react(chad);
-  }
+    const words = ['adonis', 'adonisie', 'adonisy', 'chadzie']
+    const chad = getEmojiByName(message, "chad");
+    if (words.find((word) => message.content.match(word))) {
+        if (chad) message.react(chad);
+    }
 });
 
 client.login(token);
